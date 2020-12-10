26e7e15e87464b78952fb0c6f39b26eb9828ff60
Andrew1J <andrewjuang01@gmail.com>
2020-12-10 15:12:56 -0500

    Added constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
 
+    public NoNullArrayList(){
+        super();
+    }
 
+    public NoNullArrayList(int initialCapacity){
+        super(initialCapacity);
+    }
 
     public boolean add(T element){
         if(element == null){
