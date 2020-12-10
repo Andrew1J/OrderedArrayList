7a818c6fcb23754af4cd0f4a5c57936de390839b
Andrew1J <andrewjuang01@gmail.com>
2020-12-10 14:00:21 -0500

    Added first add method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,4 +1,12 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
-    
+
+
+
+    public boolean add(T element){
+        if(element == null){
+            throw new IllegalArgumentException();
+        }
+        return super.add(element);
+    }
 }
