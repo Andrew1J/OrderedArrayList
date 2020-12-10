900abf17d54b55575ae4c65d4ec78761e7a8e0a3
Andrew1J <andrewjuang01@gmail.com>
2020-12-10 15:41:46 -0500

    Added add and set

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -17,5 +17,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
         }
         return super.add(value);
     }
-    
+
+    public void add(int index, T element){
+        add(element);
+    }
+
+    public T set (int index, T element){
+        T removed = super.remove(index);
+        add(element);
+        return removed;
+    }
 }
