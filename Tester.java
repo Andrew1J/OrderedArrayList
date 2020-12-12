b35648c992dc073798826c8f6da1819d0341e349
Andrew1J <andrewjuang01@gmail.com>
2020-12-12 17:31:14 -0500

    Added NoNullArrayList test

--- Tester.java
+++ Tester.java
@@ -1,5 +1,20 @@
 public class Tester{
     public static void main(String[] args){
+        NoNullArrayList<Integer> nonulllist = new NoNullArrayList<Integer>();
+        Integer[] list = new Integer[]{1,null,3,6,null};
+        for(Integer i:list){
+            try{
+                nonulllist.add(i);
+                System.out.println("Adding "+i);
+            } catch (IllegalArgumentException e){
+                System.out.println("Can't add nulls");
+            }
+        }
+        for(Integer i:nonulllist){
+            System.out.println(i+" ");
+        }
+        System.out.println();
 
+        
     }
 }
