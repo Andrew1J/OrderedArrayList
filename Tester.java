4e3e1efdb86d9539847a169b08730e489e9bfb23
Andrew1J <andrewjuang01@gmail.com>
2020-12-12 21:32:53 -0500

    Added tests for orderedarraylist

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
     public static void main(String[] args){
+        /*NoNullArrayList Tests
         NoNullArrayList<Integer> nonulllist = new NoNullArrayList<Integer>();
         Integer[] list = new Integer[]{1,null,3,6,null};
         for(Integer i:list){
@@ -11,10 +12,51 @@ public class Tester{
             }
         }
         for(Integer i:nonulllist){
-            System.out.println(i+" ");
+            System.out.print(i+" ");
         }
         System.out.println();
+        try{
+            nonulllist.set(2,8);
+            nonulllist.set(2,null);
+        } catch(IllegalArgumentException e){
+            System.out.println("Tried adding null");
+        }
+        for(Integer i:nonulllist){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        */
+        //OrderedArrayList Tests
+        OrderedArrayList<Integer> orderedlist = new OrderedArrayList<Integer>();
+        orderedlist.add(50);
+        orderedlist.add(30);
+        orderedlist.add(80);
+        try{
+            orderedlist.add(null);
+        } catch (IllegalArgumentException e){
+            System.out.println("Tried adding null");
+        }
+        try {
+            orderedlist.set(4,10);
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Out of bounds");
+        }
+        try {
+            orderedlist.add(2,null);
+        } catch (IllegalArgumentException e){
+            System.out.println("Tried adding null");
+        }
+        try {
+            orderedlist.set(2,null);
+        } catch (IllegalArgumentException e){
+            System.out.println("Tried adding null");
+        }
+        orderedlist.add(4,50);
+        orderedlist.add(100,34);
+        orderedlist.add(4,28);
+        for(Integer i:orderedlist){
+            System.out.print(i+" ");
+        }
 
-        
     }
 }
